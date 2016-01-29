b8e1815a247816cc8ac3e3f17242537419b7b76b
elsage <parker.spl@gmail.com>
2016-01-29 17:09:13 -0600

    All data import

--- RCode/import_clean_data.R
+++ RCode/import_clean_data.R
@@ -1,20 +1,40 @@
 # Necessary Libraries
 library(readr)
 
-
-# From https://www.divvybikes.com/data
-# THE DATA
-# Each trip is anonymized and includes:
-#   
-# Trip start day and time
+# From https://www.divvybikes.com/data: 
+#"Trip start day and time
 # Trip end day and time
 # Trip start station
 # Trip end station
 # Rider type (Member or 24-Hour Pass User)
-# If a Member trip, it will also include Member’s gender and year of birth
+# If a Member trip, it will also include Member’s gender and year of birth"
 
-# Read in the Data
-divvy_initial <- read_csv("~/Desktop/divvy_trips/Divvy_Trips_2015_07.csv",
+# Reading in data, we've got a few files, so using a function.
+read_divvy <- function(file_name) {
+  data <- read_csv(file_name,
+          col_types = cols(trip_id = col_integer(),
+                          starttime = col_date(format = "%m/%d/%Y %H:%M"),
+                          stoptime =  col_date(format = "%m/%d/%Y %H:%M"),
+                          bikeid = col_integer(),
+                          tripduration = col_integer(),
+                          from_station_id = col_integer(),
+                          from_station_name = col_character(),
+                          to_station_id = col_integer(),
+                          to_station_name = col_character(),
+                          usertype = col_character(),
+                          gender = col_character(),
+                          birthyear = col_integer()))
+  return(data)
+}
+
+# Read in data
+divvy_q3_07 <- read_divvy("~/Desktop/divvy_trips/Divvy_Trips_2015_07.csv")
+divvy_q3_08 <- read_divvy("~/Desktop/divvy_trips/Divvy_Trips_2015_08.csv")
+divvy_q3_09 <- read_divvy("~/Desktop/divvy_trips/Divvy_Trips_2015_09.csv")
+divvy_q4 <- read_divvy("~/Desktop/divvy_trips/Divvy_Trips_2015_Q4.csv")
+
+# Read in the Data, explicitly naming types.
+divvy_initial <- read_csv(file_name,
                           col_types = cols(trip_id = col_integer(),
                                       starttime = col_date(format = "%m/%d/%Y %H:%M"),
                                       stoptime =  col_date(format = "%m/%d/%Y %H:%M"),
